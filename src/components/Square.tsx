356e646877a26a797a81ae856df9d8a298b11fdd
yoshicon <shreyaskgantayet908@gmail.com>
2023-06-10 04:46:07 +0530

    remove the css import in components/Square.tsx

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,5 +1,3 @@
-import '../App.css'
-
 // The square component
 export default function Square(props: any) {
     return <button className="square" onClick={props.onSquareClick}>{props.content}</button>
